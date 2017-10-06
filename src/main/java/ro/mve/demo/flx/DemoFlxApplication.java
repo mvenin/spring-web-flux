a4d3f2d507a4d96ae9c4107e05230f39aaecec2f
VENIN Marian <marian.venin@ext.ec.europa.eu>
2017-10-06 15:21:15 +0200

    init

---
+++ src/main/java/ro/mve/demo/flx/DemoFlxApplication.java
@@ -0,0 +1,12 @@
+package ro.mve.demo.flx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFlxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFlxApplication.class, args);
+	}
+}
---
+++ src/main/java/ro/mve/demo/flx/rest/TickEventController.java
@@ -0,0 +1,20 @@
+package ro.mve.demo.flx.rest;
+
+import java.time.Duration;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class TickEventController {
+	
+	
+	@GetMapping("/tick")
+	Flux<String> tick() {
+		  return Flux.interval(Duration.ofSeconds(1)).map(x -> new Date() + " : -) "  ).log();
+	}
+
+}
